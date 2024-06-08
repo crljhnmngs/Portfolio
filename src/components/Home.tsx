999cbca4e01f2c3e10af0c275260dfbe7e43a888
crljhnmngs <manigoscarljohn@gmail.com>
2024-06-08 14:34:44 +0800

    remove start delay

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -28,7 +28,6 @@ export default function Home() {
                         ]}
                         typeSpeed={50}
                         backSpeed={60}
-                        startDelay={4000}
                         loop
                         className="font-bold home:text-5xl sm:text-5xl text-3xl text-[#3B82F6] text-center home:text-start"
                     />
