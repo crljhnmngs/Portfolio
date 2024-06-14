13c167624e1397e1c097564fd561d09b2dfa6db2
crljhnmngs <manigoscarljohn@gmail.com>
2024-06-14 22:03:29 +0800

    style: remove redundant class call

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,35 +5,34 @@ import { FaGithub, FaLinkedin, FaFacebook, FaInstagram } from 'react-icons/fa';
 import { FaSquareXTwitter } from 'react-icons/fa6';
 
 export default function Home() {
-
     const iconStyleClass: string = 'hover:scale-[1.18] size-full';
 
-    const socials =  useMemo(
+    const socials = useMemo(
         () => [
             {
                 name: 'github',
                 link: 'https://github.com/crljhnmngs',
-                icon: <FaGithub className={iconStyleClass} />
+                icon: <FaGithub className={iconStyleClass} />,
             },
             {
                 name: 'linkedin',
                 link: 'https://www.linkedin.com/in/crljhnmngs/',
-                icon: <FaLinkedin className={iconStyleClass} />
+                icon: <FaLinkedin className={iconStyleClass} />,
             },
             {
                 name: 'facebook',
                 link: 'https://www.facebook.com/crljhnmngs',
-                icon: <FaFacebook className={iconStyleClass} />
+                icon: <FaFacebook className={iconStyleClass} />,
             },
             {
                 name: 'instagram',
                 link: 'https://instagram.com/crljhnmngs',
-                icon: <FaInstagram className={iconStyleClass} />
+                icon: <FaInstagram className={iconStyleClass} />,
             },
             {
                 name: 'twitter',
                 link: 'https://twitter.com/crljhnmngs',
-                icon: <FaSquareXTwitter className={iconStyleClass} />
+                icon: <FaSquareXTwitter className={iconStyleClass} />,
             },
         ],
         []
@@ -50,7 +49,7 @@ export default function Home() {
                         Hi, I am Carl
                     </p>
                 </div>
-                <div className='mt-1'>
+                <div className="mt-1">
                     <ReactTyped
                         strings={[
                             'Software Engineer',
@@ -72,7 +71,10 @@ export default function Home() {
                 </div>
                 <div className="mt-8 flex gap-4 h-11 sm:h-[3.2rem] justify-start sm:justify-center home:justify-start w-[100%] sm:dark:text-white home:dark:text-black">
                     {socials.map((social) => (
-                        <div className="w-full h-full max-w-14" key={social.name}>
+                        <div
+                            className="w-full h-full max-w-14"
+                            key={social.name}
+                        >
                             <a
                                 href={social.link}
                                 target="_blank"
@@ -83,16 +85,16 @@ export default function Home() {
                         </div>
                     ))}
                 </div>
-                <div className="mt-8 w-1/2 flex w-full justify-start sm:justify-center home:justify-start">
+                <div className="mt-8 flex w-full justify-start sm:justify-center home:justify-start">
                     <button
                         type="button"
-                        className=" home:w-[80%] w-[50%] max-w-56 text-white h-[60px] text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-8 py-3 me-2 focus:outline-none"
+                        className=" home:w-[80%] w-[50%] max-w-56 h-[60px] text-white bg-blue-500 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-base px-8 py-3 me-2 focus:outline-none"
                     >
                         Resume
                     </button>
                 </div>
             </div>
-            <div className="sm:h-[433px] home:h-[60%] home:w-[40%] w-full flex justify-center items-start hidden sm:flex">
+            <div className="sm:h-[433px] home:h-[60%] home:w-[40%] w-full justify-center items-start hidden sm:flex">
                 <img
                     src={Person}
                     alt="placeholder"
