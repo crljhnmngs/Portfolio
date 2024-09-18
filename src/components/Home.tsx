544a34f2dd1fdc54d0d1a3023ba6c3d9fb6c8d98
Carl John Manigos <cmanigos@asi-dev2.com>
2024-09-18 09:46:23 +0800

    fix: download resume bug

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -11,7 +11,7 @@ export default function Home() {
     const memoizedData = useMemo(() => {
         return socials;
     }, []);
-    const resumeUrl = 'resume.pdf';
+    const resumeUrl = 'Resume.pdf';
 
     const handleDownload = async () => {
         try {
