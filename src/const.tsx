e049f358a9c2b920544d6b047a1bec4467c63792
crljhnmngs <manigoscarljohn@gmail.com>
2025-04-30 02:31:48 +0800

    chore: resolve conflit

--- src/const.tsx
+++ src/const.tsx
@@ -396,11 +396,7 @@ export const projects: Project[] = [
     {
         name: 'Personal FullStack Boilerplate',
         image: mern,
-<<<<<<< HEAD
-        about: 'A ready-to-use template for building scalable and secure full-stack applications. Pre-configured for efficient development, seamless API integration, and easy deployment.',
-=======
         about: 'A ready-to-use MERN template for building scalable and secure full-stack applications. Pre-configured for efficient development, seamless API integration, and easy deployment.',
->>>>>>> master
         tech: [
             'React',
             'Vite',
